95673c54c1ae976741d26366517aeb3a7cd801f0
viktoria.hrechykha <viktoryia.hrechykha@pandadoc.com>
2021-08-24 19:47:22 +0300

    new

--- test_task_4.py
+++ test_task_4.py
@@ -1,17 +1,18 @@
 import httpx
 
+
 def test():
-    url = "http://localhost:8000/task/4/"
+    url = "http://localhost:8000/task/4"
 
     with httpx.Client() as client:
-        resp = httpx.post(url, json=1)
-        assert resp.status_code = 200
-        assert resp.json = "1"
+        resp = client.post(url, json=1)
+        assert resp.status_code == 200
+        assert resp.json() == "1"
 
-        resp = httpx.post(url, json=1)
-        assert resp.status_code = 200
-        assert resp.json = "1"
+        resp = client.post(url, json=1)
+        assert resp.status_code == 200
+        assert resp.json() == "1"
 
-        resp = httpx.post(url, json="stop")
-        assert resp.status_code = 200
-        assert resp.json = 2
+        resp = client.post(url, json="stop")
+        assert resp.status_code == 200
+        assert resp.json() == 2
